c97da3ce4e81f0f090d4303f544c40e661f69816
Lindefran <slinderfran@gmail.com>
2024-10-16 13:35:19 -0300

    Aprendendo Js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
 window.alert("Ola mundo")
+
+window.console.log(
+    window.document.getElementById("titulo").innerHTML
+)
+
+console.log (
+    screen.width
+)
+
+location.href = "http://www.googlo.com" (
+
+)
+
+function voltar() {
+    history.back()
+}
+
+function adiantar() {
+    history.forward()
+}
+
+//Exemplos de comandos
+
